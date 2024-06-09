fe1e94cbc01ab8cb0c64bbb6e5d28316266c7794
Paolo <u202219186@upc.edu.pe>
2024-06-09 03:20:44 -0500

    git commit fis: Se borró una entidad que no iba en el bounded-context

--- peru_ventura_center/obj/Debug/net8.0/peru_ventura_center.AssemblyInfo.cs
+++ peru_ventura_center/obj/Debug/net8.0/peru_ventura_center.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("peru_ventura_center")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb270b42a10cb1aeee3672a3d9ff6abba51196a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9379950cbcfa9ca63d1743028dd61d0fecc3ad4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("peru_ventura_center")]
 [assembly: System.Reflection.AssemblyTitleAttribute("peru_ventura_center")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
