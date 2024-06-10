5cf9cdcf84b54b51572a628209ff0c395cfc618b
Paolo <u202219186@upc.edu.pe>
2024-06-09 19:37:54 -0500

    Update

--- peru_ventura_center/obj/Debug/net8.0/peru_ventura_center.AssemblyInfo.cs
+++ peru_ventura_center/obj/Debug/net8.0/peru_ventura_center.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("peru_ventura_center")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe1e94cbc01ab8cb0c64bbb6e5d28316266c7794")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d3f742cdbc00ee8d380f0da57cb41c45f22e29a")]
 [assembly: System.Reflection.AssemblyProductAttribute("peru_ventura_center")]
 [assembly: System.Reflection.AssemblyTitleAttribute("peru_ventura_center")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
